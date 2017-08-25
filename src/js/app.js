45c7f37ae923b4fb86d862170d4684a7fc2c3a5a
= <guy.harper@gmail.com>
2017-08-25 14:24:25 +0100

    Adding create arrow functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
-console.log('test');
+$(() => {
+
+  const $gameArea = $('.game-area');
+  const $button = $('button');
+
+  $button.on('click', () => {
+    createArrow();
+  });
+
+  function createArrow() {
+    const $newArrow = $('<div class="arrow"></div>');
+    $gameArea.append($newArrow);
+    $newArrow.css({top: 360, left: 260});
+  }
+
+});
