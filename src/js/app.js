091d944c1dbcefaca728765d2fdaba3e1388d5a3
= <guy.harper@gmail.com>
2017-08-25 14:35:20 +0100

    Creating arrow movement behaviour

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,35 @@ $(() => {
   const $button = $('button');
 
   $button.on('click', () => {
-    createArrow();
+    arrowProcess();
   });
 
   function createArrow() {
     const $newArrow = $('<div class="arrow"></div>');
     $gameArea.append($newArrow);
     $newArrow.css({top: 360, left: 260});
+    return $newArrow;
+  }
+
+  function arrowProcess() {
+    const $newArrow = createArrow();
+    const timerId = setInterval(function() {
+      moveArrow($newArrow);
+      if($newArrow.position().top === 0) {
+        deleteArrow($newArrow);
+        clearInterval(timerId);
+      }
+    },15);
+  }
+
+  function deleteArrow($arrow) {
+    $arrow.remove();
+  }
+
+  function moveArrow($arrow) {
+    const $arrowY = $arrow.position();
+    $arrow.css({top: $arrowY.top - 1});
+    return $arrowY;
   }
 
 });
